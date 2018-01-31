b2648f7fd914b98ad462b9229a85c4261e9f16aa
Samir Mehta <sammehta88@gmail.com>
2018-01-30 22:09:05 -0500

    display filename

--- src/components/App/Toolbar.js
+++ src/components/App/Toolbar.js
@@ -76,7 +76,7 @@ class Toolbar extends React.Component<{}, {}> {
       <AppBar color="default" position="static">
         <MuiToolbar>
           <Title type="title" color="inherit" onClick={goHome}>
-            My App
+            SocialCake
           </Title>
           {this.props.user && (
             <React.Fragment>
